c9c51d89eac94713ddd1a49767ef637fd7fbc541
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-06-05 02:25:35 +0530

    feat: created booking interface

---
+++ src/interfaces/bookingInterface.ts
@@ -0,0 +1,5 @@
+export interface BookingInterface extends Document {
+  studentName: string;
+  bookingTime: string;
+  sessionID: string;
+}
---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+import { StudentInterface } from './studentInterface';
+import { DeanInterface } from './deanInterface';
+import { SessionInterface } from './sessionInterface';
+import { BookingInterface } from './bookingInterface';
+
+export { StudentInterface, DeanInterface, SessionInterface, BookingInterface };
